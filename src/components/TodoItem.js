d502e84c985313be8b8ed3f2476687c62733d524
Jin Kim <golbin@gmail.com>
2023-04-11 05:18:50 +0900

    Make todo app

---
+++ src/components/TodoItem.js
@@ -0,0 +1,34 @@
+/*
+  각각의 할 일 항목을 렌더링하는 컴포넌트입니다.
+  각 할 일의 완료 상태에 따라 체크박스와 텍스트 스타일을 동기화하며,
+  삭제 버튼을 통해 해당 할 일을 삭제할 수 있습니다.
+  이 컴포넌트는 `TodoList.js`에서 사용되어 할 일 목록을 구성합니다.
+*/
+import React from "react";
+import styles from "@/styles/TodoList.module.css";
+
+// TodoItem 컴포넌트를 정의합니다.
+const TodoItem = ({ todo, onToggle, onDelete }) => {
+  // 각 할 일 항목을 렌더링합니다.
+  return (
+    <li className={styles.todoItem}>
+      {/* 체크박스를 렌더링하고, 체크박스의 상태를 할 일의 완료 상태와 동기화합니다.
+          체크박스의 상태가 변경되면 onToggle 함수를 호출하여 완료 상태를 업데이트합니다. */}
+      <input type="checkbox" checked={todo.completed} onChange={onToggle} />
+
+      {/* 할 일의 텍스트를 렌더링하고, 완료 상태에 따라 텍스트에 취소선을 적용합니다. */}
+      <span
+        className={styles.todoText}
+        style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+      >
+        {todo.text}
+      </span>
+
+      {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
+      <button onClick={onDelete}>Delete</button>
+    </li>
+  );
+};
+
+// TodoItem 컴포넌트를 내보냅니다.
+export default TodoItem;
---
+++ src/components/TodoList.js
@@ -0,0 +1,86 @@
+/* 
+  할 일 목록을 관리하고 렌더링하는 주요 컴포넌트입니다.
+  상태 관리를 위해 `useState` 훅을 사용하여 할 일 목록과 입력값을 관리합니다.
+  할 일 목록의 추가, 삭제, 완료 상태 변경 등의 기능을 구현하였습니다.
+*/
+import React, { useState } from "react";
+import TodoItem from "@/components/TodoItem";
+import styles from "@/styles/TodoList.module.css";
+
+// TodoList 컴포넌트를 정의합니다.
+const TodoList = () => {
+  // 상태를 관리하는 useState 훅을 사용하여 할 일 목록과 입력값을 초기화합니다.
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  // addTodo 함수는 입력값을 이용하여 새로운 할 일을 목록에 추가하는 함수입니다.
+  const addTodo = () => {
+    // 입력값이 비어있는 경우 함수를 종료합니다.
+    if (input.trim() === "") return;
+    // 기존 할 일 목록에 새로운 할 일을 추가하고, 입력값을 초기화합니다.
+    // {
+    //   id: 할일의 고유 id,
+    //   text: 할일의 내용,
+    //   completed: 완료 여부,
+    // }
+    // ...todos => {id: 1, text: "할일1", completed: false}, {id: 2, text: "할일2", completed: false}}, ..
+    setTodos([...todos, { id: Date.now(), text: input, completed: false }]);
+    setInput("");
+  };
+
+  // toggleTodo 함수는 체크박스를 눌러 할 일의 완료 상태를 변경하는 함수입니다.
+  const toggleTodo = (id) => {
+    // 할 일 목록에서 해당 id를 가진 할 일의 완료 상태를 반전시킵니다.
+    setTodos(
+      // todos.map((todo) =>
+      //   todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      // )
+      // ...todo => id: 1, text: "할일1", completed: false
+      todos.map((todo) => {
+        return todo.id === id ? { ...todo, completed: !todo.completed } : todo;
+      })
+    );
+  };
+
+  // deleteTodo 함수는 할 일을 목록에서 삭제하는 함수입니다.
+  const deleteTodo = (id) => {
+    // 해당 id를 가진 할 일을 제외한 나머지 목록을 새로운 상태로 저장합니다.
+    // setTodos(todos.filter((todo) => todo.id !== id));
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id !== id;
+      })
+    );
+  };
+
+  // 컴포넌트를 렌더링합니다.
+  return (
+    <div className={styles.container}>
+      <h1>Todo List</h1>
+      {/* 할 일을 입력받는 텍스트 필드입니다. */}
+      <input
+        type="text"
+        className={styles.itemInput}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      {/* 할 일을 추가하는 버튼입니다. */}
+      <button className={styles.addButton} onClick={addTodo}>
+        Add Todo
+      </button>
+      {/* 할 일 목록을 렌더링합니다. */}
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onToggle={() => toggleTodo(todo.id)}
+            onDelete={() => deleteTodo(todo.id)}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;

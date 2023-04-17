758bc9f1d9770dc386a8c39f5bdaa5af574db44a
Jin Kim <golbin@gmail.com>
2023-04-18 02:33:06 +0900

    Apply Tailwind CSS

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -56,18 +56,47 @@ const TodoList = () => {
   // 컴포넌트를 렌더링합니다.
   return (
     <div className={styles.container}>
-      <h1>Todo List</h1>
+      <h1 className="text-xl mb-4 font-bold underline underline-offset-4 decoration-wavy">
+        Todo List
+      </h1>
       {/* 할 일을 입력받는 텍스트 필드입니다. */}
       <input
         type="text"
-        className={styles.itemInput}
+        // className={styles.itemInput}
+        // -- itemInput CSS code --
+        // input[type="text"].itemInput {
+        //   width: 100%;
+        //   padding: 5px;
+        //   margin-bottom: 10px;
+        // }
+        className="w-full p-1 mb-4 border border-gray-300 rounded"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
       {/* 할 일을 추가하는 버튼입니다. */}
-      <button className={styles.addButton} onClick={addTodo}>
-        Add Todo
-      </button>
+      <div class="grid">
+        <button
+          // className={styles.addButton}
+          // -- addButton CSS code --
+          // button.addButton {
+          //   padding: 5px;
+          //   background-color: #0070f3;
+          //   color: white;
+          //   border: 1px solid #0070f3;
+          //   border-radius: 5px;
+          //   cursor: pointer;
+          // }
+          //
+          // button.addButton:hover {
+          //   background-color: #fff;
+          //   color: #0070f3;
+          // }
+          className="w-40 justify-self-end p-1 mb-4 bg-blue-500 text-white border border-blue-500 rounded hover:bg-white hover:text-blue-500"
+          onClick={addTodo}
+        >
+          Add Todo
+        </button>
+      </div>
       {/* 할 일 목록을 렌더링합니다. */}
       <ul>
         {todos.map((todo) => (
